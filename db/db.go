8c88cf9d457e8698a4d3849420708bb87955c2c5
srajalnikhra <srajalnikhra@gmail.com>
2025-09-08 19:29:54 +0530

    Correction

--- db/db.go
+++ db/db.go
@@ -11,7 +11,7 @@ type Database struct {
 }
 
 func NewDatabase() (*Database, error) {
-	db, err := sql.Open("postgres", "postgresql://root:password@localhost:5433/go-chat?sslmode=disable")
+	db, err := sql.Open("postgres", "postgresql://postgres:Nikhras%40122@localhost:5432/go_realtime_chat_app?sslmode=disable")
 	if err != nil {
 		return nil, err
 	}
